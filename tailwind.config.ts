65f7bd36a44534926a569e18494910ce53f1e469
Alfian Safrudin <alfiansafrudin436@gmail.com>
2026-06-30 11:39:28 +0700

    project configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss"
+
+export default {
+  darkMode: ["class", "media"],
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/hooks/**/*.{ts,tsx}",
+  ],
+  theme: {},
+  plugins: [],
+} satisfies Config
